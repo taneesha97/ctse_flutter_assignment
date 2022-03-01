7b7985d70e34d1b5d30ef992ab3db07d216f3b49
AnawaratneMANA <it19086408@my.sliit.lk>
2022-03-01 17:28:19 +0530

    Removed the card component. Button click working.

--- lib/components/card_custom.dart
+++ lib/components/card_custom.dart
@@ -19,72 +19,70 @@ class CustomCard extends StatelessWidget {
   final int? year;
   final Function()? press;
 
+  void testingPressFunction () {
+    print ("Button Pressed");
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Card(
-      margin: const EdgeInsets.only(
-        left: defaultPadding,
-        top: defaultPadding,
-        bottom: defaultPadding,
-      ),
-      elevation: 5,
-      child: Container(
-        width: size.width * 0.3,
-        height: double.infinity,
-        child: Column(
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight:  Radius.circular(10),
+    return Container(
+      width: size.width * 0.3,
+      height: double.infinity,
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: testingPressFunction,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(1,2),
+                    blurRadius: 10,
+                    spreadRadius: 1,
+                    color: Color(0xFF3C4046)// Add the opacity here.
+                  ),
+                ],
               ),
-              child: Image.network(
-                name,
+              child: Column(
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight:  Radius.circular(10),
+                    ),
+                    child: Image.network(
+                      name,
+                    ),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: title,
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        TextSpan(
+                          text: year.toString(),
+                          style: TextStyle(
+                            color: Color(0xFF3C4046).withOpacity(0.5),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-            GestureDetector(
-              onTap: press,
-              child: Container(
-                padding: const EdgeInsets.all(defaultPadding / 2),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: Offset(0,10),
-                      blurRadius: 50,
-                      color:  primaryColor, // Add the opacity here.
-                    ),
-                  ],
-                ),
-                child: Row(
-                  children: <Widget>[
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: title,
-                            style: Theme.of(context).textTheme.button,
-                          ),
-                          TextSpan(
-                            text: year.toString(),
-                            style: TextStyle(
-                              color: Color(0xFF3C4046).withOpacity(0.5),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
