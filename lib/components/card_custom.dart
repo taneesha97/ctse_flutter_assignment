44e1efcdbbb533013ba9774b8fab77d2a310f4da
AnawaratneMANA <it19086408@my.sliit.lk>
2022-03-01 17:12:25 +0530

    Card Component Created (Working)

--- lib/components/card_custom.dart
+++ lib/components/card_custom.dart
@@ -22,58 +22,69 @@ class CustomCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
+    return Card(
       margin: const EdgeInsets.only(
         left: defaultPadding,
-        top: defaultPadding / 2,
-        bottom: defaultPadding *2.5,
+        top: defaultPadding,
+        bottom: defaultPadding,
       ),
-
-      width: size.width * 0.4,
-      child: Column(
-        children: <Widget>[
-          Image.network(name),
-          GestureDetector(
-            onTap: press,
-            child: Container(
-              padding: const EdgeInsets.all(defaultPadding / 2),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0,10),
-                    blurRadius: 50,
-                    color:  primaryColor, // Add the opacity here.
-                  ),
-                ],
+      elevation: 5,
+      child: Container(
+        width: size.width * 0.3,
+        height: double.infinity,
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight:  Radius.circular(10),
               ),
-              child: Row(
-                children: <Widget>[
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: title,
-                          style: Theme.of(context).textTheme.button,
-                        ),
-                        TextSpan(
-                          text: year.toString(),
-                          style: TextStyle(
-                            color: primaryColor.withOpacity(0.5),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+              child: Image.network(
+                name,
               ),
             ),
-          )
-        ],
+            GestureDetector(
+              onTap: press,
+              child: Container(
+                padding: const EdgeInsets.all(defaultPadding / 2),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0,10),
+                      blurRadius: 50,
+                      color:  primaryColor, // Add the opacity here.
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: <Widget>[
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: title,
+                            style: Theme.of(context).textTheme.button,
+                          ),
+                          TextSpan(
+                            text: year.toString(),
+                            style: TextStyle(
+                              color: Color(0xFF3C4046).withOpacity(0.5),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
