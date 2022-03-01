0cb1759d211cc649a22dbbe9eab50d1ace9b7cb0
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2022-03-01 19:58:38 +0530

    Add the file structure changed

--- lib/components/quiz/body.dart
+++ lib/components/quiz/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'options.dart';
+
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
 
@@ -68,43 +70,5 @@ class Body extends StatelessWidget {
         ],
       ),
     ));
-  }
-}
-
-class Options extends StatelessWidget {
-  const Options({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.only(bottom: 10),
-        child: Material(
-          borderRadius: BorderRadius.circular(15),
-          color: Color.fromARGB(255, 0, 238, 255),
-          child: InkWell(
-            borderRadius: BorderRadius.circular(25),
-            child: Container(
-              margin: EdgeInsets.only(bottom: 8),
-              padding: EdgeInsets.only(top: 8, bottom: 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 10),
-                    child: Text(
-                      "Goosebumps",
-                      style: TextStyle(color: Colors.black45, fontSize: 16),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
