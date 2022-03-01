e1b04f210f1e146dcf690d1f101ac84bf343a80e
AnawaratneMANA <it19086408@my.sliit.lk>
2022-03-01 15:03:14 +0530

    Attributes created for Card.

--- lib/components/card_custom.dart
+++ lib/components/card_custom.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CustomCard extends StatelessWidget {
-  const CustomCard({Key? key}) : super(key: key);
+  const CustomCard({
+    // Input Parameters.
+    Key? key,
+    this.image,
+    this.title,
+    this.year,
+    this.press,
+
+  }) : super(key: key);
+
+  // Attributes. (Card)
+  final String? image, title;
+  final int? year;
+  final Function? press;
 
   @override
   Widget build(BuildContext context) {
