b55e779e4644d1257582fdd618ecd9111a7f1071
Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>
2022-03-02 16:47:55 +0530

    Progress bar added

--- lib/components/audio_quizes/audio_quiz_body.dart
+++ lib/components/audio_quizes/audio_quiz_body.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:audioplayers/audioplayers.dart';
+import 'package:ctse_assignment_1/components/progressbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -108,89 +109,96 @@ class _AudioQuizBodyState extends State<AudioQuizBody> {
                   height: 8,
                 ),
                 Container(
-                  child: Text(
-                    currentpostlabel,
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ),
-                Container(
-                    child: Slider(
-                  value: double.parse(currentpos.toString()),
-                  min: 0,
-                  max: double.parse(maxduration.toString()),
-                  divisions: maxduration,
-                  label: currentpostlabel,
-                  onChanged: (double value) async {
-                    int seekval = value.round();
-                    int result =
-                        await player.seek(Duration(milliseconds: seekval));
-                    if (result == 1) {
-                      //seek successful
-                      currentpos = seekval;
-                    } else {
-                      print("Seek unsuccessful.");
-                    }
-                  },
-                )),
-                Container(
-                  child: Wrap(
-                    spacing: 10,
+                  child: Column(
                     children: [
-                      ElevatedButton.icon(
-                          onPressed: () async {
-                            if (!isplaying && !audioplayed) {
-                              int result = await player.playBytes(audiobytes);
-                              if (result == 1) {
-                                //play success
-                                setState(() {
-                                  isplaying = true;
-                                  audioplayed = true;
-                                });
-                              } else {
-                                print("Error while playing audio.");
-                              }
-                            } else if (audioplayed && !isplaying) {
-                              int result = await player.resume();
-                              if (result == 1) {
-                                //resume success
-                                setState(() {
-                                  isplaying = true;
-                                  audioplayed = true;
-                                });
-                              } else {
-                                print("Error on resume audio.");
-                              }
-                            } else {
-                              int result = await player.pause();
-                              if (result == 1) {
-                                //pause success
-                                setState(() {
-                                  isplaying = false;
-                                });
-                              } else {
-                                print("Error on pause audio.");
-                              }
-                            }
-                          },
-                          icon:
-                              Icon(isplaying ? Icons.pause : Icons.play_arrow),
-                          label: Text(isplaying ? "Pause" : "Play")),
-                      ElevatedButton.icon(
-                          onPressed: () async {
-                            int result = await player.stop();
-                            if (result == 1) {
-                              //stop success
-                              setState(() {
-                                isplaying = false;
-                                audioplayed = false;
-                                currentpos = 0;
-                              });
-                            } else {
-                              print("Error on stop audio.");
-                            }
-                          },
-                          icon: Icon(Icons.stop),
-                          label: Text("Stop")),
+                      Container(
+                        child: Text(
+                          currentpostlabel,
+                          style: TextStyle(fontSize: 25),
+                        ),
+                      ),
+                      Container(
+                          child: Slider(
+                        value: double.parse(currentpos.toString()),
+                        min: 0,
+                        max: double.parse(maxduration.toString()),
+                        divisions: maxduration,
+                        label: currentpostlabel,
+                        onChanged: (double value) async {
+                          int seekval = value.round();
+                          int result = await player
+                              .seek(Duration(milliseconds: seekval));
+                          if (result == 1) {
+                            //seek successful
+                            currentpos = seekval;
+                          } else {
+                            print("Seek unsuccessful.");
+                          }
+                        },
+                      )),
+                      Container(
+                        child: Wrap(
+                          spacing: 10,
+                          children: [
+                            ElevatedButton.icon(
+                                onPressed: () async {
+                                  if (!isplaying && !audioplayed) {
+                                    int result =
+                                        await player.playBytes(audiobytes);
+                                    if (result == 1) {
+                                      //play success
+                                      setState(() {
+                                        isplaying = true;
+                                        audioplayed = true;
+                                      });
+                                    } else {
+                                      print("Error while playing audio.");
+                                    }
+                                  } else if (audioplayed && !isplaying) {
+                                    int result = await player.resume();
+                                    if (result == 1) {
+                                      //resume success
+                                      setState(() {
+                                        isplaying = true;
+                                        audioplayed = true;
+                                      });
+                                    } else {
+                                      print("Error on resume audio.");
+                                    }
+                                  } else {
+                                    int result = await player.pause();
+                                    if (result == 1) {
+                                      //pause success
+                                      setState(() {
+                                        isplaying = false;
+                                      });
+                                    } else {
+                                      print("Error on pause audio.");
+                                    }
+                                  }
+                                },
+                                icon: Icon(
+                                    isplaying ? Icons.pause : Icons.play_arrow),
+                                label: Text(isplaying ? "Pause" : "Play")),
+                            ElevatedButton.icon(
+                                onPressed: () async {
+                                  int result = await player.stop();
+                                  if (result == 1) {
+                                    //stop success
+                                    setState(() {
+                                      isplaying = false;
+                                      audioplayed = false;
+                                      currentpos = 0;
+                                    });
+                                  } else {
+                                    print("Error on stop audio.");
+                                  }
+                                },
+                                icon: Icon(Icons.stop),
+                                label: Text("Stop")),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -201,7 +209,11 @@ class _AudioQuizBodyState extends State<AudioQuizBody> {
                   itemCount: 4,
                   shrinkWrap: true,
                   itemBuilder: (context, index) => Options(),
-                )
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                ProgressBar(),
               ],
             ),
           ),
