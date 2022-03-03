76298bfa22feb1ae24d6c2d0157eb8be99b9fcd1
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2022-03-04 01:13:51 +0530

    Index page navigation added

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,15 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        backgroundColor: Colors.black,
       ),
-      initialRoute: '/',
+      // initialRoute: '/',
       routes: {
         // When navigating to the "/" route, build the FirstScreen widget.
-        '/': (context) => const MovieWiki(),
+        '/movieWiki': (context) => const MovieWiki(),
         // When navigating to the "/second" route, build the SecondScreen widget.
-        '/second': (context) => QuizScreen(),
+        '/quizScreen': (context) => QuizScreen(),
       },
-      home: IndexPage(),
+      home: const IndexPage(),
     );
   }
 }
--- lib/screens/index_page.dart
+++ lib/screens/index_page.dart
@@ -8,69 +8,76 @@ class IndexPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red, // background
-                onPrimary: Colors.white,
-                padding: const EdgeInsets.all(20.0),
-                fixedSize: const Size(640, 70),
-                // foreground
+    return Container(
+      color: Colors.yellow,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red, // background
+                  onPrimary: Colors.white,
+                  padding: const EdgeInsets.all(20.0),
+                  fixedSize: const Size(640, 70),
+                  // foreground
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/movieWiki');
+                },
+                child: Text('Movie Wiki Page', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
               ),
-              onPressed: () { },
-              child: Text('Movie Wiki Page', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.teal, // background
-                onPrimary: Colors.white,
-                padding: const EdgeInsets.all(20.0),
-                fixedSize: const Size(640, 70),
-                // foreground
+              SizedBox(
+                height: 15,
               ),
-              onPressed: () { },
-              child: Text('Single Movie Page', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.teal, // background
-                onPrimary: Colors.white,
-                padding: const EdgeInsets.all(20.0),
-                fixedSize: const Size(640, 70),
-                // foreground
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.teal, // background
+                  onPrimary: Colors.white,
+                  padding: const EdgeInsets.all(20.0),
+                  fixedSize: const Size(640, 70),
+                  // foreground
+                ),
+                onPressed: () { },
+                child: Text('Single Movie Page', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
               ),
-              onPressed: () { },
-              child: Text('Movie Wiki Page', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red, // background
-                onPrimary: Colors.white,
-                padding: const EdgeInsets.all(20.0),
-                fixedSize: const Size(640, 70),
-                // foreground
+              SizedBox(
+                height: 15,
               ),
-              onPressed: () { },
-              child: Text('Movie Wiki Page', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-          ],
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.teal, // background
+                  onPrimary: Colors.white,
+                  padding: const EdgeInsets.all(20.0),
+                  fixedSize: const Size(640, 70),
+                  // foreground
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/quizScreen');
+                },
+                child: Text('Quiz Screen', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red, // background
+                  onPrimary: Colors.white,
+                  padding: const EdgeInsets.all(20.0),
+                  fixedSize: const Size(640, 70),
+                  // foreground
+                ),
+                onPressed: () { },
+                child: Text('Movie Wiki Page', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+            ],
+          ),
         ),
       ),
     );
