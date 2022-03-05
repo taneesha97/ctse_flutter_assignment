291030a2c9aea9d70075bb72a2a2cb675333b893
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2022-03-06 01:28:26 +0530

    movie list view created
    Signed-off-by: taneesha97 <taneeshaayeshmanthi97@gmail.com>

--- lib/screens/quize_list.dart
+++ lib/screens/quize_list.dart
@@ -17,6 +17,60 @@ class _QuizeListState extends State<QuizeList> {
           style: TextStyle(color: Colors.black),
         ),
       ),
+      body: ListView.builder(
+          itemCount: 10,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) => Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+            child: Card(
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0.0),
+              ),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.symmetric(horizontal: 3.0, vertical: 3.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children:<Widget> [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children:<Widget> [
+                        SizedBox(width: 5.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children:<Widget> [
+                            Text(category[index], style: TextStyle(color: Colors.black, fontSize: 18.0, fontWeight: FontWeight.bold)),
+                            Text(time[index], style: TextStyle(color: Colors.grey)),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      // padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                      width: 55.0,
+                      height: 55.0,
+                      margin: EdgeInsets.all(2),
+                      color: Colors.blue,
+                      child: Center(
+                          child: Text.rich(
+                            TextSpan(
+                                text: "76pts",
+                                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white )
+                            ),
+                          )
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+      ),
 
     );
   }
