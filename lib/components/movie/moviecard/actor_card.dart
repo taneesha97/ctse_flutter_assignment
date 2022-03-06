0483c65923c51b857575a153d94a3008751c5451
AnawaratneMANA <it19086408@my.sliit.lk>
2022-03-06 13:36:48 +0530

    Switching from the Master Branch.

---
+++ lib/components/movie/moviecard/actor_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class ActorCard extends StatelessWidget {
+  const ActorCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
