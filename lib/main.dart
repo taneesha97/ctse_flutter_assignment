1dd408c6f9b091ce375f94e55cdff53889b16dc4
AnawaratneMANA <it19086408@my.sliit.lk>
2022-03-07 21:06:20 +0530

    pubspec file updated.

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,11 @@ import 'package:ctse_assignment_1/screens/score_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
-
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   runApp(const MyApp());
-// }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
