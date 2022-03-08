5f5c9d1e62f5b41837970c5a0bfde85825326539
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2022-03-08 14:00:33 +0530

    change padding sizes
    Signed-off-by: taneesha97 <taneeshaayeshmanthi97@gmail.com>

--- lib/screens/quize_list.dart
+++ lib/screens/quize_list.dart
@@ -27,74 +27,79 @@ class _QuizeListState extends State<QuizeList> {
           style: TextStyle(color: Colors.black),
         ),
       ),
-      body: ListView.builder(
-          itemCount: 10,
-          shrinkWrap: true,
-          itemBuilder: (BuildContext context, int index) => Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-            child: Card(
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(0.0),
-              ),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.symmetric(horizontal: 3.0, vertical: 3.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children:<Widget> [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children:<Widget> [
-                        SizedBox(width: 5.0),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children:<Widget> [
-                            Text(category[index], style: TextStyle(color: Colors.black, fontSize: 18.0, fontWeight: FontWeight.bold)),
-                            Text(time[index], style: TextStyle(color: Colors.grey)),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(5),
-                      child: Row(
-                        children: [
-                          Ink(
-                            decoration: ShapeDecoration(
-                                shape: CircleBorder(), color: Colors.blue),
-                            child: IconButton(
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.add_chart,
-                                  color: Colors.white,
-                                )),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Ink(
-                            decoration: ShapeDecoration(
-                                shape: CircleBorder(), color: Colors.red),
-                            child: IconButton(
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.delete,
-                                  color: Colors.white,
-                                )),
+      body: Container(
+        color: Colors.black12,
+        child: ListView.builder(
+            itemCount: 10,
+            shrinkWrap: true,
+            itemBuilder: (BuildContext context, int index) => Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.symmetric(horizontal: 2.0, vertical: 1.0),
+              child: Card(
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6.0),
+                ),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(horizontal: 3.0, vertical: 3.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children:<Widget> [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children:<Widget> [
+                          SizedBox(width: 5.0),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children:<Widget> [
+                              Text(category[index], style: TextStyle(color: Colors.black, fontSize: 18.0, fontWeight: FontWeight.bold)),
+                              Text(time[index], style: TextStyle(color: Colors.grey)),
+                            ],
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                      Container(
+                        padding: EdgeInsets.all(1),
+                        child: Row(
+                          children: [
+                            Ink(
+                              decoration: ShapeDecoration(
+                                  shape: CircleBorder(), color: Colors.blue),
+                              child: IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.start,
+                                    color: Colors.white,
+                                    size: 15,
+                                  )),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Ink(
+                              decoration: ShapeDecoration(
+                                  shape: CircleBorder(), color: Colors.red),
+                              child: IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.delete,
+                                    color: Colors.white,
+                                    size: 15,
+                                  )),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
 
+        ),
       ),
 
     );
