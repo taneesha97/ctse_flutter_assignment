0d3d66bca7fe2a874177c9c0045d51911afa9815
AnawaratneMANA <it19086408@my.sliit.lk>
2022-03-08 12:00:00 +0530

    API class Updated.

--- lib/util/api.dart
+++ lib/util/api.dart
@@ -2,21 +2,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
 class Api {
+  final String path;
+  CollectionReference? ref;
   late FirebaseFirestore firestore;
-  final Stream<QuerySnapshot> _usersStream =
-      FirebaseFirestore.instance.collection('users').snapshots();
 
-  Future<StreamBuilder<QuerySnapshot<Object?>>> read() async {
-    return StreamBuilder<QuerySnapshot>(
-        stream: _usersStream,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Something went wrong');
-          }
+  Api(this.path){
+    ref = firestore.collection(path);
+  }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text("Loading");
-          }
-        });
+   Future<QuerySnapshot>?getDataCollection()  {
+     return ref?.get();
   }
 }
