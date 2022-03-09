7bc23ed3f58abed009eed67fd0ab4f06c1e94c62
AnawaratneMANA <it19086408@my.sliit.lk>
2022-03-09 18:08:03 +0530

    InkWell Added to Actor Card.

--- lib/util/crud_model.dart
+++ lib/util/crud_model.dart
@@ -3,6 +3,13 @@ import 'package:flutter/cupertino.dart';
 
 class CrudModel extends ChangeNotifier{
 
+  //Testing
+  String _testString = "Test Value 1";
+
+  CrudModel(){
+    this._testString = "Test Value 2";
+  }
+
   // Steam Data For the Movies.
   final Stream<QuerySnapshot> _movieStream = FirebaseFirestore.instance.collection('movies').snapshots();
 
@@ -10,4 +17,10 @@ class CrudModel extends ChangeNotifier{
   Stream<QuerySnapshot> get movies {
     return _movieStream;
   }
+
+  // Getter for the private testing variable.
+  String  get getTestString{
+    return _testString;
+  }
+
 }
