f94a186c260cc8d607dc57b6a0c1a97851712adf
AnawaratneMANA <it19086408@my.sliit.lk>
2022-03-14 15:23:16 +0530

    Stateful class created movie library form.

---
+++ lib/screens/movie_library_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LibraryForm extends StatefulWidget {
+  const LibraryForm({Key? key}) : super(key: key);
+
+  @override
+  _LibraryFormState createState() => _LibraryFormState();
+}
+
+class _LibraryFormState extends State<LibraryForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
