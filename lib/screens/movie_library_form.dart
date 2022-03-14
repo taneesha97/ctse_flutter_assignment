3d7d9c08d0fc98a911af60ffd414c1bebaece6d2
AnawaratneMANA <it19086408@my.sliit.lk>
2022-03-14 16:48:06 +0530

    Add Libraries to the Firebase Form created.

--- lib/screens/movie_library_form.dart
+++ lib/screens/movie_library_form.dart
@@ -10,11 +10,13 @@ class LibraryForm extends StatefulWidget {
 
 class _LibraryFormState extends State<LibraryForm> {
   final formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   String name = "";
 
   @override
   Widget build(BuildContext context) {
-    final double height= MediaQuery.of(context).size.height;
+    final double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.teal,
@@ -25,23 +27,53 @@ class _LibraryFormState extends State<LibraryForm> {
         padding: EdgeInsets.all(10),
         child: Container(
           child: Form(
+            key: formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Create a Library", style: Styles.textSectionHeader,),
-                Text("Custom Libraries to Manage Favorite Movies", style: Styles.textSectionSubBody,),
+                Text(
+                  "Create a Library",
+                  style: Styles.textSectionHeader,
+                ),
+                Text(
+                  "Custom Libraries to Manage Favorite Movies",
+                  style: Styles.textSectionSubBody,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 TextFormField(
                   decoration: const InputDecoration(
-                    labelText: "Enter the Library Name"
-                  ),
-                  validator: (value){
-                    if(value!.isEmpty || !RegExp(r'^[a-z]+$').hasMatch(value!)){
+                      labelText: "Enter the Library Name"),
+                  validator: (value) {
+                    if (value!.isEmpty ||
+                        !RegExp(r'^[a-z]+$').hasMatch(value!)) {
                       return "Please enter correct library name";
                     } else {
                       return null;
                     }
                   },
-                )
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+                Center(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          final status = SnackBar(
+                              content: Text(
+                            "Library Added",
+                            style: Styles.textSectionSubBody,
+                          ));
+                          _scaffoldKey.currentState!.showSnackBar(status);
+                        }
+                      },
+                      child: Text(
+                        "Add to the Library",
+                        style: Styles.navBarTitle,
+                      )),
+                ),
               ],
             ),
           ),
