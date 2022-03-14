d746a917842e46e7ae5d1c3c66a4691971c2bdff
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2022-03-14 11:15:43 +0530

    problem fixed
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- lib/util/crud_model.dart
+++ lib/util/crud_model.dart
@@ -2,23 +2,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ctse_assignment_1/models/movie.dart';
 import 'package:flutter/cupertino.dart';
 
-class CrudModel extends ChangeNotifier{
-
-  final Stream<QuerySnapshot> _movieStream = FirebaseFirestore.instance.collection('movies').snapshots();
+class CrudModel extends ChangeNotifier {
+  final Stream<QuerySnapshot> _movieStream =
+      FirebaseFirestore.instance.collection('movies').snapshots();
 
   // Testing Constructor.
 
-  CrudModel();
-  CrudModel(){
+  CrudModel() {
     TestingMethod();
   }
 
   //Testing
   String _testString = "Test Value 1";
 
-
   // Steam Data For the Movies.
-
 
   // Getter for the User Steam.
   Stream<QuerySnapshot> get movies {
@@ -27,27 +24,25 @@ class CrudModel extends ChangeNotifier{
 
   // Getter for the private testing variable.
 
-  Stream<List<Movie>>  get getListOfMovies{
-
-    return _movieStream.map((event) => event.docs.map((e) => Movie(
-        id: e.get("id"),
-        title: e.get("name"),
-        imageUrl: e.get("title"),
-        description: e.get("id"),
-        rating: e.get("id"),
-        year: e.get("id"),
-        duration: e.get("id"),
-    )).toList());
+  Stream<List<Movie>> get getListOfMovies {
+    return _movieStream.map((event) => event.docs
+        .map((e) => Movie(
+              id: e.get("id"),
+              title: e.get("name"),
+              imageUrl: e.get("title"),
+              description: e.get("id"),
+              rating: e.get("id"),
+              year: e.get("id"),
+              duration: e.get("id"),
+            ))
+        .toList());
   }
 
-  void TestingMethod(){
+  void TestingMethod() {
     Stream<List<Movie>> movies = getListOfMovies;
     print("This is List of Movies From CrudModel - $movies");
   }
 
-
-
   // Steam a List of Books from (Method).
-
 
 }
