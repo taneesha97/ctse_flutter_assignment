6c7c68363db583ac3b2179605df14bb65e4358c5
AnawaratneMANA <it19086408@my.sliit.lk>
2022-03-16 05:19:49 +0530

    Lost Actor Card Recovered.

--- lib/components/movie/moviecard/actor_card.dart
+++ lib/components/movie/moviecard/actor_card.dart
@@ -1,12 +1,65 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../models/movie.dart';
+import '../../../styles.dart';
 
 class ActorCard extends StatelessWidget {
   const ActorCard({Key? key}) : super(key: key);
+  final String imageUrl = "https://nettv4u.com/imagine/t/e/l/u/g/u/telugu-actress-eesha-hot-pictures-68.jpg";
 
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      margin: EdgeInsets.all(6),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.4),
+            spreadRadius: 2,
+            blurRadius: 8,
+          ),
+        ],
+        color: Colors.white,
+        borderRadius: new BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: InkWell(
+          onTap: (){},
+          child: Row(
+            children: [
+              Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.4),
+                          spreadRadius: 2,
+                          blurRadius: 8,
+                        ),
+                      ],
+                      borderRadius: new BorderRadius.circular(100),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(imageUrl),
+                      )
+                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Actor Name", style: Styles.actorCardHeader, overflow: TextOverflow.ellipsis,),
+                    Text("Character Name", style: TextStyle(fontSize: 12),),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/util/crud_model.dart
+++ lib/util/crud_model.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
-
 import '../models/library_model.dart';
 import '../models/movie.dart';
 
