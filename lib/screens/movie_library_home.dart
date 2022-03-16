d6f6c1a91248534cad87eb6e0e777fc37ccee711
AnawaratneMANA <it19086408@my.sliit.lk>
2022-03-16 05:47:27 +0530

    Library Home File added.

--- lib/screens/movie_library_home.dart
+++ lib/screens/movie_library_home.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../models/movie.dart';
+import '../styles.dart';
 
 class LibraryHome extends StatefulWidget {
   final String name;
-  const LibraryHome({Key? key, required this.name}) : super(key: key);
+  final Stream<List<Movie>> movies;
+  const LibraryHome({Key? key, required this.name, required this.movies}) : super(key: key);
 
   @override
   State<LibraryHome> createState() => _LibraryHomeState();
@@ -11,8 +16,51 @@ class LibraryHome extends StatefulWidget {
 class _LibraryHomeState extends State<LibraryHome> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Container(
+        child:
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "All Libraries",
+              style: Styles.textSectionHeader,
+            ),
+            Text(
+              "Custom Libraries to Manage Favorite Movies",
+              style: Styles.textSectionSubBody,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Flexible(
+              flex: 9,
+              child: SafeArea(
+                child: StreamBuilder<List<Movie>>(
+                    stream: widget.movies,
+                    builder: (context, snapshot) {
+                      if(snapshot.hasError){
+                        return Text("Error");
+                      } else if(snapshot.hasData){
+                        final libraries = snapshot.data!;
+                        return ListView(
 
+                        );
+                      } else {
+                        return Center(child: CircularProgressIndicator(),);
+                      }
+                    }
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
