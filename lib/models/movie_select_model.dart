a72cb4e4b35de146edda7fb0c9969a284ea0c135
AnawaratneMANA <it19086408@my.sliit.lk>
2022-03-17 14:20:01 +0530

    Selected Movie Model Attribtues updated.

--- lib/models/movie_select_model.dart
+++ lib/models/movie_select_model.dart
@@ -1,11 +1,18 @@
 class SelectedMovieModel {
   String title, imageUrl;
   int year;
+  String description;
   bool isSelected;
+  String rating;
+  String duration;
+
 
   SelectedMovieModel(
     this.title,
     this.imageUrl,
+    this.description,
+    this.rating,
+    this.duration,
     this.isSelected,
     this.year,
   );
