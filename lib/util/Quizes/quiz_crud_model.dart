1623282c38814909d421df0fbe7984c941c1da65
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2022-03-17 16:41:28 +0530

    chnages updated
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- lib/util/Quizes/quiz_crud_model.dart
+++ lib/util/Quizes/quiz_crud_model.dart
@@ -120,6 +120,21 @@ class QuizCrudModel extends ChangeNotifier {
     }
   }
 
+  Future<dynamic> insertFeedBack(String value1, String value2) async {
+    try {
+      DocumentReference<Map<String, dynamic>> value =
+      await FirebaseFirestore.instance.collection('Feedback').add({
+        'expression': value1 ?? '',
+        'Comment': value2 ?? '',
+      });
+      // print(value.id);
+      // print('dkks');
+      // return value.id.toString();
+    } catch (e) {
+      print(e);
+    }
+  }
+
   // Getter for the User Steam.
   Stream<QuerySnapshot> get quizesList {
     return _quizList;
