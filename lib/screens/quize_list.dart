d2ef0e599661e916a0f17f2567e0816d179edcb0
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2022-03-26 23:23:48 +0530

    header added quiz list
    Signed-off-by: taneesha97 <taneeshaayeshmanthi97@gmail.com>

--- lib/screens/quize_list.dart
+++ lib/screens/quize_list.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../components/list_view/card_view.dart';
 import '../models/quize_list_model.dart';
+import '../styles.dart';
 import '../util/QuizeConfig/quize_list_crud_model.dart';
 
 class QuizeList extends StatefulWidget {
@@ -41,23 +42,53 @@ class _QuizeListState extends State<QuizeList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Quize List",
-          style: TextStyle(color: Colors.black),
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          elevation: 0,
+          toolbarHeight: 10,
+        ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+        Padding(
+        padding: const EdgeInsets.only(
+          left: 10,
+          right: 10,
+          top: 10,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Quiz List",
+              style: Styles.textSectionHeader,
+            ),
+            Text(
+              "Saved quiz Presets",
+              style: Styles.textSectionSubBody,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
         ),
       ),
-      body: Container(
-        color: Colors.black12,
-        child: ListView.builder(
-            itemCount: docs1.length,
-            shrinkWrap: true,
-            itemBuilder: (BuildContext context, int index) =>
-                CardView1(index: index, text1 : docs1[index].category.toString() , text2: int.parse(docs1[index].time.toString()), id: docs1[index].id.toString(), questions: int.parse(docs1[index].questions.toString())),
-        ),
-      ),
+            Container(
+              color: Colors.black12,
+              child: ListView.builder(
+                itemCount: docs1.length,
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, int index) =>
+                    CardView1(index: index, text1 : docs1[index].category.toString() , text2: int.parse(docs1[index].time.toString()), id: docs1[index].id.toString(), questions: int.parse(docs1[index].questions.toString())),
+              ),
+            ),
+      ]
 
-    );
+
+    )));
   }
 }
 
