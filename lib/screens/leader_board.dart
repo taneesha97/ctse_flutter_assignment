b5648be7753611f85862e6e4483a33d831d6b6e4
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2022-03-27 13:23:36 +0530

    remove unnecessary codes in leader_board.dart
    Signed-off-by: taneesha97 <taneeshaayeshmanthi97@gmail.com>

--- lib/screens/leader_board.dart
+++ lib/screens/leader_board.dart
@@ -1,9 +1,7 @@
 import 'package:ctse_assignment_1/components/leaderboard/leaderboard_card.dart';
-import 'package:ctse_assignment_1/screens/quize_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
-
 import '../models/leaderboard_model.dart';
 import '../styles.dart';
 import '../util/QuizeConfig/leaderboard_crud_model.dart';
@@ -16,8 +14,6 @@ class LeaderBoard extends StatefulWidget {
 
   }) : super(key: key);
 
-
-
   @override
   _LeaderBoardState createState() => _LeaderBoardState();
 
@@ -27,15 +23,10 @@ class _LeaderBoardState extends State<LeaderBoard> {
 
   List<LeaderBoardModel> docs1= [];
 
-
-  List names = ["Bright vachirawit", "Bright vachirawit","Bright vachirawit","Bright vachirawit"];
-  List place = ["1st place", "1st place","1st place","1st place"];
-
   @override
   void initState() {
     super.initState();
 
-    // Previous.
     Provider.of<LeaderBoardCrudModel>(context, listen: false)
         .readLeaderBoard().then((value) => {
       setState(() {
@@ -44,61 +35,11 @@ class _LeaderBoardState extends State<LeaderBoard> {
     });
   }
 
-  bool _isShown = true;
-
-  void _deleteItem(BuildContext context) {
-    showDialog(
-        context: context,
-        builder: (BuildContext ctx) {
-          return AlertDialog(
-            title: const Text('Please Confirm'),
-            content: const Text('Are you sure to remove the item?'),
-            actions: [
-              // The "Yes" button
-              TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                    print(widget.id);
-                    Provider.of<LeaderBoardCrudModel>(context, listen: false).deleteLeaderboardItem(widget.id.toString());
-                    //initState();
-                    // Navigator.push(
-                    //     context,
-                    //     MaterialPageRoute(
-                    //         builder: (context) => const LeaderBoard(id: "x8MbVVeKSJtPXkspQPn4",)));
-                  },
-                  child: const Text('Yes')),
-              TextButton(
-                  onPressed: () {
-                    // Close the dialog
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('No'))
-            ],
-          );
-        });
-  }
-
   @override
   Widget build(BuildContext context) {
     // Stream Version.
     Stream<List<LeaderBoardModel>> list = Provider.of<LeaderBoardCrudModel>(context, listen: false)
         .getListOfLeaderBoxes;
-
-    Color getTheRightColor(int index) {
-      print(index);
-      if (docs1[index].version.toString() == 'Gold') {
-          return Colors.orange;
-      }
-      else if(docs1[index].version.toString() == 'Silver'){
-          return Colors.black38;
-          // return Colors.amber;
-      }
-      else if(docs1[index].version.toString() == 'Bronze'){
-          return Colors.redAccent;
-      }
-      return Colors.green;
-
-    }
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.teal,
@@ -125,10 +66,6 @@ class _LeaderBoardState extends State<LeaderBoard> {
                   "LeaderBoard",
                   style: Styles.textSectionHeader,
                 ),
-                // Text(
-                //   "Add movies to the library",
-                //   style: Styles.textSectionSubBody,
-                // ),
                 const SizedBox(
                   height: 10,
                 ),
@@ -156,8 +93,6 @@ class _LeaderBoardState extends State<LeaderBoard> {
                 }
               }
           ),
-
-
         ]))
     );
   }
