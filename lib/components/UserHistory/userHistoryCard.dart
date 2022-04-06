9400c3fd12cbec5ed6ca012603418db689c2dd42
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2022-04-06 20:21:38 +0530

    User history delete completed
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- lib/components/UserHistory/userHistoryCard.dart
+++ lib/components/UserHistory/userHistoryCard.dart
@@ -1,14 +1,45 @@
 import 'package:ctse_assignment_1/models/result_quiz.dart';
 import 'package:ctse_assignment_1/models/user_history_model.dart';
+import 'package:ctse_assignment_1/util/Quiz_Result/quiz_result_crud_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../screens/user_history.dart';
 
 class UserHistoryCard extends StatelessWidget {
 
   final int index;
-  //final bool _isShown = true;
+  final bool _isShown = true;
   final ResultQuiz model;
   const UserHistoryCard({Key? key, required this.model, required this.index}) : super(key: key);
 
+
+  void _deleteItem(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext ctx) {
+          return AlertDialog(
+            title: const Text('Please Confirm'),
+            content: const Text('Are you sure to remove the item?'),
+            actions: [
+              // The "Yes" button
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context, rootNavigator: true).pop(); // New Pop Method.
+                    Provider.of<QuizResultCrudModel>(context, listen: false).deleteQuizResult(model.id.toString());
+                  },
+                  child: const Text('Yes')),
+              TextButton(
+                  onPressed: () {
+                    // Close the dialog
+                    Navigator.of(context).pop();
+
+                  },
+                  child: const Text('No'))
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,6 +72,25 @@ class UserHistoryCard extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+            Container(
+                margin: EdgeInsets.only(left: 5.0),
+                child: Row(
+                    children: [
+                      Ink(
+                        decoration: const ShapeDecoration(
+                            shape: CircleBorder(), color: Colors.white),
+                        child: IconButton(
+                           onPressed: _isShown == true ? () => _deleteItem(context) : null,
+                            //onPressed: (){},
+                            icon: const Icon(
+                              Icons.delete,
+                              color: Colors.black,
+                              size: 20,
+                            )),
+                      ),
+                    ]
+                )
             ),
             Container(
               alignment: Alignment.center,
--- lib/util/UserHistory/user_history_crud_model.dart
+++ lib/util/UserHistory/user_history_crud_model.dart
@@ -36,35 +36,54 @@ class UserHistoryCrudModel extends ChangeNotifier {
     }
   }
 
-  Future<dynamic> readUserHistoryDetails(String uID) async {
-    QuerySnapshot querySnapshot;
-    List<ResultQuiz> docs1 = [];
-    try {
-      querySnapshot = await _db
-          .collection('result-quizes')
-          .where('userId', isEqualTo: uID)
-          .get();
-      if (querySnapshot.docs.isNotEmpty) {
-        for (var doc in querySnapshot.docs.toList()) {
-          ResultQuiz b = ResultQuiz(
-              id: doc.id,
-              correct_answer: doc['correct_answer'],
-              no_questions: doc['no_questions'],
-              wrong_answer: doc['wrong_answer'], //correct_points
-              userId: doc['userId'].toString(),
-              correct_points: doc['correct_points'],
-              date: doc['date'].toString(),
-              category: doc['category'].toString()
-          );
-          //
+  // Future<dynamic> readUserHistoryDetails(String uID) async {
+  //   QuerySnapshot querySnapshot;
+  //   List<ResultQuiz> docs1 = [];
+  //   try {
+  //     querySnapshot = await _db
+  //         .collection('result-quizes')
+  //         .where('userId', isEqualTo: uID)
+  //         .get();
+  //     if (querySnapshot.docs.isNotEmpty) {
+  //       for (var doc in querySnapshot.docs.toList()) {
+  //         ResultQuiz b = ResultQuiz(
+  //             id: doc.id,
+  //             correct_answer: doc['correct_answer'],
+  //             no_questions: doc['no_questions'],
+  //             wrong_answer: doc['wrong_answer'], //correct_points
+  //             userId: doc['userId'].toString(),
+  //             correct_points: doc['correct_points'],
+  //             date: doc['date'].toString(),
+  //             category: doc['category'].toString()
+  //         );
+  //         //
+  //
+  //         docs1.add(b);
+  //       }
+  //       return docs1;
+  //     }
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
 
-          docs1.add(b);
-        }
-        return docs1;
-      }
-    } catch (e) {
-      print(e);
-    }
+  Stream<List<ResultQuiz>> readUserHistoryDetails(String uID) {
+    Stream<QuerySnapshot> streams =
+    FirebaseFirestore.instance.collection('result-quizes')
+        .where('userId', isEqualTo: uID).snapshots();
+
+    return streams.map((event) => event.docs
+        .map((doc) => ResultQuiz(
+        id: doc.id,
+        correct_answer: doc['correct_answer'],
+        no_questions: doc['no_questions'],
+        wrong_answer: doc['wrong_answer'], //correct_points
+        userId: doc['userId'].toString(),
+        correct_points: doc['correct_points'],
+        date: doc['date'].toString(),
+        category: doc['category'].toString()
+    ))
+        .toList());
   }
 
 
